2954e918d2aec55e154bbde1bfccf9383b615c0b
ngechu <ngechu08@gmail.com>
2024-05-31 22:20:17 +0300

    cleanup final algo

--- introduction.js
+++ introduction.js
@@ -4,22 +4,15 @@ function twoSum(arr, outcome) {
 	//Initialize an empty obj
 	let obj = {}
 	let index = 0;
-	let indexes = []
 	for (let i in arr) {
 		const diff = outcome - arr[i]
-
 		if (diff in obj) {
-			indexes = [...indexes, Number(obj[diff]), Number(i)]
+			return [obj[diff], i]
 		} else {
 			obj[arr[i]] = index
 			index++
 
 		}
 	}
-	console.log('=========', indexes)
-
-	return indexes
 }
-
-
-twoSum([2, 5, 1, 7], 6)
+console.log(twoSum([2, 5, 1, 7], 6))
